d9e9d3fd6966b24c205bad5ebade0a529ae39b75
Federico Cangiotti </>
2023-10-21 16:27:24 +0200

    feat: add player controls

--- Scenes/Subscenes/Player.gd
+++ Scenes/Subscenes/Player.gd
@@ -1,23 +1,89 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-var directionX
-var directionY
+var wheel_base = 70
+var steering_angle = 30 #15
+var engine_power = 900
+var friction = -55
+var drag = -0.06
+var braking = -450
+var max_speed_reverse = 300
+var slip_speed = 400
+var traction_fast = 2.5
+var traction_slow = 10
+
+var acceleration = Vector2.ZERO
+var steer_direction
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	directionX = Input.get_axis("ui_left", "ui_right")
-	if directionX:
-		velocity.x = directionX * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	directionY = Input.get_axis("ui_up", "ui_down")
-	if directionY:
-		velocity.y = directionY * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	acceleration = Vector2.ZERO
+	get_input()
+	apply_friction(delta)
+	calculate_steering(delta)
+	velocity += acceleration * delta
 	move_and_slide()
+	
+func apply_friction(delta):
+	if acceleration == Vector2.ZERO and velocity.length() < 50:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction * delta
+	var drag_force = velocity * velocity.length() * drag * delta
+	acceleration += drag_force + friction_force
+	
+func get_input():
+	var turn = Input.get_axis("steer_left", "steer_right")
+	steer_direction = turn * deg_to_rad(steering_angle)
+	if Input.is_action_pressed("accelerate"):
+		acceleration = transform.x * engine_power
+	if Input.is_action_pressed("brake"):
+		acceleration = transform.x * braking
+	
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_direction) * delta
+	var new_heading = rear_wheel.direction_to(front_wheel)
+	var traction = traction_slow
+	if velocity.length() > slip_speed:
+		traction = traction_fast
+	var d = new_heading.dot(velocity.normalized())
+	if d > 0:
+		velocity = lerp(velocity, new_heading * velocity.length(), traction * delta)
+	if d < 0:
+		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+#	velocity = new_heading * velocity.length()
+	rotation = new_heading.angle()
+
+"""
+var wheel_base = 70  # Distance from front to rear wheel
+var steering_angle = 15  # Amount that front wheel turns, in degrees
+
+var steer_direction
+
+func get_input():
+	var turn = Input.get_axis("steer_left", "steer_right")
+	steer_direction = turn * deg_to_rad(steering_angle)
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("accelerate"):
+		velocity = transform.x * 500
+	if Input.is_action_pressed("brake"):
+		velocity = transform.x / 500
+
+func calculate_steering(delta):
+	# 1. Find the wheel positions
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	# 2. Move the wheels forward
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_direction) * delta
+	# 3. Find the new direction vector
+	var new_heading = rear_wheel.direction_to(front_wheel)
+	# 4. Set the velocity and rotation to the new direction
+	velocity = new_heading * velocity.length()
+	rotation = new_heading.angle()
+
+func _physics_process(delta):
+	get_input()
+	calculate_steering(delta)
+	move_and_slide()
+"""
