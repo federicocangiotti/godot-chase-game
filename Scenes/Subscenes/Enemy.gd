ac2709168df86382ef7598eea3d9995ad2bfb698
Federico Cangiotti </>
2023-10-22 18:23:37 +0200

    feat: disable enemy chase for specified time

--- Scenes/Subscenes/Enemy.gd
+++ Scenes/Subscenes/Enemy.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var collision_thread = Thread.new()
 var chase = false
 var targeted_entity = null
 var speed = 150
@@ -25,3 +26,26 @@ func _on_player_detection_body_exited(body):
 func _on_player_damage_body_entered(body):
 	if body.name == "Player":
 		body.health -= 1
+		$CollisionShape2D.disabled = true
+		$PlayerDetection/CollisionShape2D.disabled = true
+		$PlayerDamage/CollisionShape2D.disabled = true
+		
+		disable_collision()
+		active_collision()
+
+func disable_collision():
+	$".".set_collision_layer(0)
+	$".".set_collision_mask(0)
+
+	$PlayerDetection.monitoring = false
+	$PlayerDetection/CollisionShape2D.disabled = true
+	$PlayerDamage/CollisionShape2D.disabled = true
+
+func active_collision():
+	await get_tree().create_timer(3).timeout
+	
+	$".".set_collision_layer(1)
+	$".".set_collision_mask(1)
+	$PlayerDetection.monitoring = true
+	$PlayerDetection/CollisionShape2D.disabled = false
+	$PlayerDamage/CollisionShape2D.disabled = false
