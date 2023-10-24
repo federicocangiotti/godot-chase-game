dae51da36704c2eca62a838b23045d2dea6b9410
EdoardoUbaldi <edoubba@gmail.com>
2023-10-24 11:30:04 +0200

    EnemyRotation-and-Fixes

--- Scenes/Subscenes/Enemy.gd
+++ Scenes/Subscenes/Enemy.gd
@@ -4,21 +4,27 @@ const speed = 100
 
 @export var player: Node2D
 @onready var Nav_agent := $NavigationAgent2D as NavigationAgent2D
-@export var steer_force = 50.0
-
-var target = null
+#@export var steer_force = 50.0
+#
+#var target = null
 
 func _physics_process(_delta: float) -> void:
 	var dir = to_local(Nav_agent.get_next_path_position()).normalized()
 	velocity = dir * speed
-	#rotation da sistemare
+	Nav_agent.target_position = player.global_position
+	$Area2D/Sprite2D.global_rotation = $Area2D/Sprite2D.global_position.angle_to_point(Nav_agent.target_position)
 	move_and_slide()
 
-
-func makepath() -> void:
-	Nav_agent.target_position = player.global_position
-
-
-func _on_timer_timeout() -> void:
-	makepath()
+func _on_area_2d_body_entered(body):
+	print("PROVA")
+	queue_free()
+	$"../Window".visible = true
+	
 	pass # Replace with function body.
+
+#func _on_timer_timeout() -> void:
+#	makepath()
+#	pass # Replace with function body. t
+
+
+
--- Scripts/Playground/btnBack.gd
+++ Scripts/Playground/btnBack.gd
@@ -1,19 +1,26 @@
 extends Button
 
+var pause = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 # popup button
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Options.tscn")
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
 	
-func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_ESCAPE:
-			get_tree().change_scene_to_file("res://Scenes/Options.tscn")
+#func _unhandled_input(event):
+#	if event is InputEventKey:
+#		if event.pressed and event.keycode == KEY_ESCAPE:
+#			if not pause:
+#				pause = true
+#				set_physics_process(false)
+#				$Options.show()
+#			else:
+#				pause = false
+#				set_physics_process(true)
+#				$Options.hide()
