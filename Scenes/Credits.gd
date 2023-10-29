2cc09699d819a284b8e0c6f8c92d24281308ca0f
EdoardoUbaldi <edoubba@gmail.com>
2023-10-29 20:59:19 +0100

    Credits-menu

---
+++ Scenes/Credits.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -22,3 +22,8 @@ func _on_options_pressed():
 func _on_quit_pressed():
 	get_tree().quit()
 	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
+	pass # Replace with function body.
