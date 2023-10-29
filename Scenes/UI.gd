7a84d169812eb930277c1a3e09237835bc230957
EdoardoUbaldi <edoubba@gmail.com>
2023-10-29 18:31:43 +0100

    Score-Counter

--- Scenes/UI.gd
+++ Scenes/UI.gd
@@ -1,16 +1,19 @@
-extends Node2D
+extends CanvasLayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Score()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+
+func Score():
 	var score = 0
 	while($"../Window".visibility_changed):
 		score += 1
-		 = convert(score, TYPE_STRING)
-	pass
-
+		$"../Score".Text = convert(score, 4)
