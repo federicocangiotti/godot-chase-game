a8ffa81489a1c3dd056e6742eba78e12a8e91833
EdoardoUbaldi <edoubba@gmail.com>
2023-11-09 12:31:54 +0100

    Camera-Fix

---
+++ Scenes/Subscenes/Camera.gd
@@ -0,0 +1,31 @@
+extends Camera2D
+
+const BOUND_X_MIN = -1920
+const BOUND_X_MAX = 1920
+const BOUND_Y_MIN = -1080
+const BOUND_Y_MAX = 1080
+
+const CAMERA_LIMIT_X = 100
+const CAMERA_LIMIT_Y = 100
+
+#Get min and max bounds
+const MIN_X = BOUND_X_MIN + CAMERA_LIMIT_X
+const MIN_Y = BOUND_Y_MIN + CAMERA_LIMIT_Y
+const MAX_X = BOUND_X_MAX - CAMERA_LIMIT_X
+const MAX_Y = BOUND_Y_MAX - CAMERA_LIMIT_Y
+
+func _edge():
+	var camera = self.position
+
+	if camera.x < MIN_X:
+		camera.x = MIN_X
+	if camera.x > MAX_X:
+		camera.x = MAX_X
+	if camera.y < MIN_Y:
+		camera.y = MIN_Y
+	if camera.y > MAX_Y:
+		camera.y = MAX_Y
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
